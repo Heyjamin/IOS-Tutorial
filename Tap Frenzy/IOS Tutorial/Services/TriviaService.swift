027ae8cee0abdeadd5128a4933ef91cf50d036ad
Nuwan Jeewantha <amilajeewantha@icloud.com>
2026-07-01 20:55:31 +0530

    Create TriviaService using async/await

--- Tap Frenzy/IOS Tutorial/Services/TriviaService.swift
+++ Tap Frenzy/IOS Tutorial/Services/TriviaService.swift
@@ -5,3 +5,23 @@
 //  Created by G P M A Nuwan Jeewantha on 2026-07-01.
 //
 
+import Foundation
+
+struct TriviaService {
+    private let endpoint = "https://opentdb.com/api.php?amount=10&type=multiple"
+    
+    func fetchQuestions() async throws -> [TriviaQuestion] {
+        
+        guard let url = URL(string: endpoint) else {
+            throw URLError(.badURL)
+        }
+        
+        let (data, _) = try await URLSession.shared.data(from:url)
+        
+        let response = try JSONDecoder().decode(
+            TriviaResponse.self,
+            from: data
+        )
+        return response.results
+    }
+}
