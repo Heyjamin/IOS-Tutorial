a5b0335bc91d6ccf7d28c373e203bf5f9f739fd4
Nuwan Jeewantha <amilajeewantha@icloud.com>
2026-07-25 12:25:14 +0530

    Class Day

--- Tap Frenzy/IOS Tutorial/Components/AnswerButton.swift
+++ Tap Frenzy/IOS Tutorial/Components/AnswerButton.swift
@@ -80,7 +80,7 @@ struct AnswerButton: View {
                 isCorrect: true,
                 correctAnswer: nil,
                 compact: true
-            ) {}
+            ){}
         }
         .padding()
     }
